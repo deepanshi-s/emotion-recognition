1e763b0926582b4674f1cbb8d88b4f3bfd2dd764
Manan Sharma <manansharma296@gmail.com>
2018-12-09 20:27:34 +0530

    Created speech_features.py

---
+++ speech_features.py
@@ -0,0 +1,30 @@
+#NOTE: Assume that the frame received by the function is already windowed
+import numpy as np
+import scipy.signal as sig
+import scipy.io.wavfile as wav
+
+def pitch(frame,Fs):
+    '''
+    A short description about the function
+    '''
+
+
+def zcr(frame,Fs):
+    '''
+    A short description about the function
+    '''
+
+
+def teo(frame,Fs):
+    '''
+    A short description about the function
+    '''
+
+
+def energy(frame,Fs):
+    '''
+    A short description about the function
+    '''
+
+
+
