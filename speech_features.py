0ebdfdd5b10758cf50ba1ce95749017a51dee72f
Deepanshi <deepanshi.asr.21@gmail.com>
2018-12-13 23:57:08 +0530

    teo and energy codes added

--- speech_features.py
+++ speech_features.py
@@ -16,14 +16,26 @@ def zcr(frame,Fs):
 
 
 def teo(frame,Fs):
+    size=len(frame)
+    te=np.zeros(size-2)
+    te[0]=0
+    for i in range(1,size-1,1):
+        te[i]=frame[i]*frame[i]-frame[i-1]*frame[i+1]
+    return(te)
     '''
-    A short description about the function
+    calculates the non linear energy of the signal.
     '''
 
 
 def energy(frame,Fs):
+    size=len(frame)
+    E=0
+    for i in range (0,size,1):
+        E=E+frame[i]*frame[i]
+    E1=np.log10(E)
+    return(E1)
     '''
-    A short description about the function
+    calcultes the net energy of the signal.
     '''
 
 
